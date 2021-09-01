333914d6c1bd69221dcb21d759d3a505cdbb6811
hcelante <higor.celante@gmail.com>
2021-08-31 23:40:58 -0300

    novidades, input, pair programming

--- front/pages/components/graph/index.js
+++ front/pages/components/graph/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import  Chart  from "react-google-charts";
+import Chart from "react-google-charts";
 
 async function getData() {
     const url = "http://127.0.0.1:5000/"
@@ -7,6 +7,7 @@ async function getData() {
     const result = await response.json()
     return result
 }
+
 
 const graphStyle = {
     backgroundColor: 'white'
@@ -16,12 +17,20 @@ function Graph() {
     const [charged, setCharged] = useState(0);
     const [myData, setMyData] = useState({});
     const [sentiments, setSentiments] = useState([]);
+    const [value, setValue] = useState(0);
 
+    async function collect(){
+        const url = "http://127.0.0.1:5000/collect/" + value
+        fetch(url).then(() =>{
+            console.log('response')
+        })
 
+    }
+
+    
     function getGraph() {
         getData().then(response => {
             setMyData(response)
-
             countSentiments(response)
         })
 
@@ -42,27 +51,38 @@ function Graph() {
 
     console.log(sentiments)
     return (
-        <div style={graphStyle}>
+        <div>
             <div>
-                <div className="">
-                <Chart
-                    width={'1000px'}
-                    height={'1000px'}
-                    chartType="PieChart"
-                    loader={<div>Carregando Gráfico</div>}
-                    data={[
-                        ['Sentimento', 'Tweets'],
-                        ['Positivo', sentiments[1]],
-                        ['Negativo', sentiments[0]],
-                        ['Neutro', sentiments[2]],
-                    ]}
-                    options={{
-                        title: 'Análise Sentimental de tweets sobre o personagem Luffy de One Piece',
-                        // Just add this option
-                        is3D: true,
-                    }}
-                    rootProps={{ 'data-testid': '2' }}
-                ></Chart>
+                <label>
+                    <h2>Número de Tweets que deseja buscar:</h2>
+                    <input type="text" value={value} onChange={e =>  setValue(e.target.value)} />
+                </label>
+                <button onClick={() => collect()}>
+                    Update
+                </button>
+            </div>
+            <div style={graphStyle}>
+                <div>
+                    <div className="">
+                        <Chart
+                            width={'1000px'}
+                            height={'1000px'}
+                            chartType="PieChart"
+                            loader={<div>Carregando Gráfico</div>}
+                            data={[
+                                ['Sentimento', 'Tweets'],
+                                ['Positivo', sentiments[1]],
+                                ['Negativo', sentiments[0]],
+                                ['Neutro', sentiments[2]],
+                            ]}
+                            options={{
+                                title: 'Análise Sentimental de tweets sobre o personagem Luffy de One Piece',
+                                // Just add this option
+                                is3D: true,
+                            }}
+                            rootProps={{ 'data-testid': '2' }}
+                        ></Chart>
+                    </div>
                 </div>
             </div>
         </div>
